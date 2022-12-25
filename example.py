a0f5ce041f37fb41483300991763e6c3755d6222
PratyushGupta0 <gpratyush20@iitk.ac.in>
2022-12-26 03:28:53 +0530

    Complete wrapper for now

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 from protocol import Protocol
 
 talker = Protocol("192.168.4.1", 23)
-
 talker.arm()
-while True:
-    talker.set_RPY_THR(roll=1500, pitch=1500, yaw=1500, thrust=900)
+talker.takeoff()
+talker.land() 
